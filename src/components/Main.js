ee97f12730381399c43e75f9122cf9cc7daadc5f
Sunny Prakash <sunny.prakashgm@gmail.com>
2019-03-15 01:02:31 +0530

    added Page not found component

--- src/components/Main.js
+++ src/components/Main.js
@@ -2,12 +2,14 @@ import React from "react";
 import { Switch, Route } from "react-router-dom";
 import Home from "./Home";
 import Roster from "./Roster";
+import PageNotFound from './PageNotFound';
 
 const Main = () => (
   <main>
     <Switch>
       <Route exact path="/" component={Home} />
       <Route path="/roster" component={Roster} />
+      <Route path="*" exact component={PageNotFound}/>
     </Switch>
   </main>
 );
